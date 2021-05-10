7194a4893efec8f53d824a99cfc475c0d4948030
crejk <crejk@ctemplar.com>
2021-05-10 16:42:02 +0200

    add test case for multipart body

--- src/test/java/com/crejk/filehosting/UploadTest.java
+++ src/test/java/com/crejk/filehosting/UploadTest.java
@@ -2,8 +2,6 @@ package com.crejk.filehosting;
 
 import com.crejk.filehosting.base.IntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 
 import java.util.UUID;
@@ -27,5 +25,23 @@ public class UploadTest extends IntegrationTest {
         // then
         result.expectStatus().isOk()
                 .expectBodyList(UUID.class).hasSize(1);
+    }
+
+    @Test
+    public void shouldReturnIdsOfUploadedFiles() {
+        // given
+        MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
+        bodyBuilder.part("file", TEXT_FILE);
+        bodyBuilder.part("file", TEXT_FILE);
+
+        // when
+        var result = webTestClient.post()
+                .uri("/upload/")
+                .bodyValue(bodyBuilder.build())
+                .exchange();
+
+        // then
+        result.expectStatus().isOk()
+                .expectBodyList(UUID.class).hasSize(2);
     }
 }
